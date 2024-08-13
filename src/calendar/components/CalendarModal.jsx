63b641e897c218046820a6b61c245a7f8ab070f3
Jose Escribano <jescribanomora@gmail.com>
2024-08-13 13:00:22 +0200

    Add content to calendar modal

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -30,9 +30,58 @@ export const CalendarModal = () => {
             overlayClassName="modal-background"
             closeTimeoutMS={200}
         >
-            <h1>Hello world!</h1>
+            <h1> Nuevo evento </h1>
             <hr />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <form className="container">
+                <div className="form-group mb-2">
+                    <label>Date and starting hour</label>
+                    <input
+                        className="form-control"
+                        placeholder="Staring date"
+                    />
+                </div>
+
+                <div className="form-group mb-2">
+                    <label>Date and ending hour</label>
+                    <input className="form-control" placeholder="Ending date" />
+                </div>
+
+                <hr />
+                <div className="form-group mb-2">
+                    <label>Title and notes</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name of the event"
+                        name="title"
+                        autoComplete="off"
+                    />
+                    <small id="emailHelp" className="form-text text-muted">
+                        Short description
+                    </small>
+                </div>
+
+                <div className="form-group mb-2">
+                    <textarea
+                        type="text"
+                        className="form-control"
+                        placeholder="Notes"
+                        rows="5"
+                        name="notes"
+                    ></textarea>
+                    <small id="emailHelp" className="form-text text-muted">
+                        Additional information
+                    </small>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary btn-block"
+                >
+                    <i className="far fa-save"></i>
+                    <span> Save</span>
+                </button>
+            </form>
         </Modal>
     );
 };
