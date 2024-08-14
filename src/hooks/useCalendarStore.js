f79d727081a685175c6501f21cfc0deba129f093
Jose Escribano <jescribanomora@gmail.com>
2024-08-14 13:00:38 +0200

    Implement update calendar event

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {
     onAddNewEvent,
     onSetActiveEvent,
+    onUpdateEvent,
 } from "../store/calendar/calendarSlice";
 
 export const useCalendarStore = () => {
@@ -13,8 +14,8 @@ export const useCalendarStore = () => {
     };
 
     const startSavingEvent = async (calendarEvent) => {
-        if (calendarEvent.id) {
-            //
+        if (calendarEvent._id) {
+            dispatch(onUpdateEvent({ ...calendarEvent }));
         } else {
             dispatch(
                 onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() })
--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -29,7 +29,15 @@ export const calendarSlice = createSlice({
             state.events.push(payload);
             state.activeEvent = null;
         },
+        onUpdateEvent: (state, { payload }) => {
+            state.events = state.events.map((event) => {
+                if (event._id === payload._id) return payload;
+
+                return event;
+            });
+        },
     },
 });
 
-export const { onSetActiveEvent, onAddNewEvent } = calendarSlice.actions;
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent } =
+    calendarSlice.actions;
